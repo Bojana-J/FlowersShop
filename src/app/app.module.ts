87be7e3760d512d90b48bc166f60d3bf11c7a561
Bojana-J <b.janjicic@gmail.com>
2018-03-04 01:39:43 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FlowersComponent } from './flowers/flowers.component';
+import { FlowerListComponent } from './flowers/flower-list/flower-list.component';
+import { FlowerInfoComponent } from './flowers/flower-info/flower-info.component';
+import { FlowerItemComponent } from './flowers/flower-list/flower-item/flower-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FlowersComponent,
+    FlowerListComponent,
+    FlowerInfoComponent,
+    FlowerItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+ 
+   
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
